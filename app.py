572eb5b65dc8754360fc0a1e6ff14f2c2290d676
AL WAHAB <arifqayyum2@email.com>
2026-06-07 12:46:31 -0700

    fix syntax error

--- app.py
+++ app.py
@@ -596,7 +596,7 @@ HTML = """
 <!DOCTYPE html>
 <html>
 <head>
-    <title>🤖 Trading Bot</title>
+    <title>Advanced Trading Bot</title>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <style>
